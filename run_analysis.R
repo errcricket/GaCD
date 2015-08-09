c7d1602797724cacf597f778af01700af10b6a86
reichenberger <cricket@reichenbergers-MacBook-Pro.local>
2015-08-09 00:18:29 -0400

    added (back) ddply???!!!! and rounded numeric values from final file

--- run_analysis.R
+++ run_analysis.R
@@ -57,7 +57,7 @@ complete_training <- data.frame(subject_id = subjects$subjects, activity = info[
 complete_training$phase[1:7352] <- 'training'
 
 # Load training data
-###training_X <- read.table('temp', header=F)
+#training_X <- read.table('../UCI_HAR_Dataset/train/temp.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
 training_X <- read.table('../UCI_HAR_Dataset/train/X_train_original.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
 feature_list <- as.character(feature_labels$V2)
 names(training_X) <- feature_list
@@ -70,8 +70,6 @@ training_X <- training_X[,col.num]
 complete_training <- cbind(complete_training, training_X)
 attr(complete_training, 'df_name') <- 'complete_training' #must do this after each r/c(bind)
 #print_col_row(complete_training)
-
-#write.table(complete_training, file='tidy_project_data.txt', row.name=FALSE, sep=',', na='NA')
 
 ############## TESTING #######################
 # Loading testing subjects. There are 7532 observations 
@@ -97,6 +95,7 @@ complete_testing$phase[1:ncol(subjects_testing)] <- 'testing'
 
 # Load testing data
 testing_X <- read.table('../UCI_HAR_Dataset/test/X_test.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
+#testing_X <- read.table('../UCI_HAR_Dataset/test/temp.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
 feature_list <- as.character(feature_labels$V2)
 names(testing_X) <- feature_list
 
@@ -112,76 +111,20 @@ attr(complete_testing, 'df_name') <- 'complete_testing' #must do this after each
 
 complete_df <- rbind(complete_testing, complete_training)
 attr(complete_df, 'df_name') <- 'complete_df' #must do this after each r/c(bind)
-print_col_row(complete_df)
-#write.table(complete_df, file='tidy_project_data.txt', row.name=F, sep=' ', na='NA')
-#attach(complete_df)
-#col.keep <- which(colnames(complete_df) %in% kept_features)
+#print_col_row(complete_df)
 complete_mean <- data.frame()
 
-#kept_features <- factor(unique(unlist(desired_features$saved_features, use.names = FALSE)))
-#  ActivityID           Activity
-#1          1            WALKING
-#2          2   WALKING_UPSTAIRS
-#3          3 WALKING_DOWNSTAIRS
-#4          4            SITTING
-#5          5           STANDING
-#6          6             LAYING
-
-start = 0
-end = 0
 for (i in 1:30)
 {
 	for (a in factor(unique(unlist(activity_labels$Activity, use.names = FALSE))))
 	{
 		temp <- data.frame()
-		print(a)
-		#temp <- complete_df[complete_df$subject_id == i & complete_df$activity == a]
-		#print(temp)
-		#complete_mean <- rbind(complete_mean, temp)
+		temp <- complete_df[complete_df$subject_id == i & complete_df$activity == a, ]
+		temp2 <- data.frame()
+		temp2 <- ddply(temp, .(activity), numcolwise(mean))
+		complete_mean <- rbind(complete_mean, temp2)
 	}
-	#print(temp)
-	#print(ddply(temp, .(activity), numcolwise(mean)))
-	#print(str(ddply(temp, .(activity), numcolwise(mean))))
-#	start <- end + 1
-#	end <- ncol(activity_labels) + start - 1 #ncol(activity_labels) = 6
-#	complete_mean[start:end, ] <- ddply(temp, .(activity), numcolwise(mean))
-	#print(sapply(temp[, 4:82], mean, na.rm=TRUE))
-	#print(ddply(complete_df, .(activity), summarize))
 }
 	
-#complete_mean
-#write.table(complete_mean, file='tidy_project_data.txt', row.name=F, sep=' ', na='NA') #print(temp)
-#ddply(complete_df, .(subject_id, activity), summarize)
-#write.table(split(complete_df, list(complete_df$subject_id, complete_df$activity)), file='split_data.txt', row.name=F, sep=' ', na='NA')
-#by(complete_df[, 4:82], subject_id, activity, colMeans)
-#complete_subjects <- data.frame()
-#complete_subjects <- split(complete_df, as.factor(complete_df$activity), drop = FALSE)
-#names(complete_subjects) #[1] "LAYING"             "SITTING"            "STANDING"           "WALKING"            "WALKING_DOWNSTAIRS" "WALKING_UPSTAIRS"  
-#by(complete_subjects[, 4:82], activity, colMeans)
-#by(complete_df[, 4:82], subject_id, colMeans)
-#"subject_id"                      "activity"                        "phase"
-
-#complete_df
-
-#write.table(complete_training, file='tidy_project_data.txt', row.name=FALSE, sep=',', na='NA')
-#write.table(split(complete_df, as.factor(complete_df$subject_id), drop = FALSE), file='tidy_project_data.txt', row.name=F, sep=' ', na='NA')
-#complete_subjects <- data.frame()
-#complete_subjects <- split(complete_df, as.factor(complete_df$subject_id), drop = FALSE)
-#complete_activities <- split(complete_df, as.factor(complete_df$activity), drop = FALSE)
-#write.table(complete_activities, file='tidy_project_data.txt', row.name=F, sep=' ', na='NA')
-#complete_activities
-#complete_subjects
-#mean_df <- lapply(complete_df[, 4:82], FUN=mean, na.rm=T)
-#print_col_row(complete_df)
-
-#"subject_id"                      "activity"                        "phase" 
-#selected <- names(complete_df)
-#selected <- selected[-c(1, 2, 3)]
-#selected
-#selected <- selected[, 4:82]
-#selected
-
-#complete_avg <- sapply(complete_df[,4:79],mean, na.rm=TRUE)
-#complete_avg
-#complete_avg <- ddply(complete_df, .(subject_id, activity, phase), summarize, MeanGC=mean(GC, na.rm=TRUE) )
-#write.table(complete_training, file='tidy_project_data.txt', row.name=FALSE, sep=',', na='NA')
+complete_mean[] <- lapply(complete_mean, function(x) {if (is.numeric(x)) round(x, 4) else x}) #round values to 4 digits IF numeric
+write.table(complete_mean, file='tidy_project_data.txt', row.name=F, sep=' ', na='NA') #print(temp)
