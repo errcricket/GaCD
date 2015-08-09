7d1eaa603af830510bf0f23f9f5bcbdb040f7097
reichenberger <cricket@reichenbergers-MacBook-Pro.local>
2015-08-09 12:47:00 -0400

    replaced for loop with melt and dcast

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 options(width=9999)
 library(plyr)
+library(reshape2)
 
 # Get only the mean and standard deviation features from feature.txt file
 #grep mean ../UCI_HAR_Dataset/features.txt | awk '{print $2}' > mean.txt
@@ -28,14 +29,14 @@ testing_y <- read.csv('../UCI_HAR_Dataset/test/y_test.txt', header=F, sep=' ') #
 names(training_y) <- 'activity'
 names(testing_y) <- 'activity'
 
-# Load training data
+#4. Appropriately labels the data set with descriptive variable names & loading training/testing data
 training_X <- read.table('../UCI_HAR_Dataset/train/X_train_original.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
 testing_X <- read.table('../UCI_HAR_Dataset/test/X_test.txt', header=F) #must not use sep = (' ', ',') or the # of rows/columns will be messed up.
 feature_list <- as.character(feature_labels$V2)
 names(training_X) <- feature_list
 names(testing_X) <- feature_list
 
-# Using matching function (pgs. 47-48 Hadley Advanced R) to marry activity description names to file of activity numbers
+#3. Uses descriptive activity names to name the activities in the data set # Using matching function (pgs. 47-48 Hadley Advanced R) to marry activity description names to file of activity numbers
 info_training <- data.frame(activity_id = activity_labels$ActivityID, activity_desc = activity_labels$Activity)
 info_testing <- data.frame(activity_id_testing = activity_labels$ActivityID, activity_desc = activity_labels$Activity)
 id_training <- match(training_y$activity, info_training$activity_id) #info[id, 2] will give me the column with activity description names only
@@ -45,7 +46,7 @@ id_testing <- match(testing_y$activity, info_testing$activity_id_testing) #info_
 complete_training <- data.frame(subject_id = subjects_training$subjects, activity = info_training[id_training,2])
 complete_testing <- data.frame(subject_id = subjects_testing$subjects, activity = info_testing[id_testing,2])
 
-# Select columns from dataframe that are related to mean and standard deviation
+#2. Extracts only the measurements on the mean and standard deviation for each measurement. # Select columns from dataframe that are related to mean and standard deviation
 col.num <- which(colnames(training_X) %in% kept_features)
 training_X <- training_X[,col.num]
 col.num <- which(colnames(testing_X) %in% kept_features) #in theory, this should be identical to previous col.num
@@ -55,20 +56,11 @@ testing_X <- testing_X[,col.num]
 complete_training <- cbind(complete_training, training_X)
 complete_testing <- cbind(complete_testing, testing_X)
 
+#1. Merges the training and the test sets to create one data set. 
 complete_df <- rbind(complete_testing, complete_training)
-complete_mean <- data.frame()
 
-for (i in 1:30) #need way to get rid of this for loop
-{
-	for (a in factor(unique(unlist(activity_labels$Activity, use.names = FALSE))))
-	{
-		temp <- data.frame()
-		temp <- complete_df[complete_df$subject_id == i & complete_df$activity == a, ]
-		temp2 <- data.frame()
-		temp2 <- ddply(temp, .(activity), numcolwise(mean))
-		complete_mean <- rbind(complete_mean, temp2)
-	}
-}
-	
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+complete_melt <- melt(complete_df, id=c('subject_id', 'activity'))
+complete_mean <- dcast(complete_melt, subject_id+activity ~ variable, mean)
 complete_mean[] <- lapply(complete_mean, function(x) {if (is.numeric(x)) round(x, 4) else x}) #round values to 4 digits IF numeric
 write.table(complete_mean, file='tidy_project_data.txt', row.name=F, sep=' ', na='NA') #print(temp)
